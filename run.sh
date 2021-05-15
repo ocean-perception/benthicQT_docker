b5cc8518abc3d554622fa44f9f43009cf9d5490d
Miquel Massot <miquel.massot@gmail.com>
2021-05-15 22:15:25 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker run -p 6080:80 --rm -it -v $(pwd):/host oceanperception/benthicqt
+if [[ $# -eq 0 ]] ; then
+    echo "Usage: $0 HOSTPATH"
+    echo "Provide the absolute path to the folder where the meshes are stored,"
+    echo "then open your browser at open http://127.0.0.1:6080/."
+    exit 0
+fi
+
+HOSTPATH=$1
+
+docker run -p 6080:80 --rm -it -v $(HOSTPATH):/host oceanperception/benthicqt
